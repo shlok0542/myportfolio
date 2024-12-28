95a8ec0b012d123169056d5f6a345808a297834a
e <shlokmaury0542a376@gmail.com>
2024-12-28 09:58:51 +0530

    index.js file added for index.html

---
+++ index.js
@@ -0,0 +1,8 @@
+let resume=document.querySelector(".resume");
+let project=document.querySelector(".project");
+resume.addEventListener("click",()=>{
+    window.location.href="resume.html";
+});
+resume.addEventListener("click",()=>{
+    window.location.href="project.html";
+});
