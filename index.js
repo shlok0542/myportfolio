e6b3ccc8482a1b38cd4fd0b3d76e03124871ba99
e <shlokmaury0542a376@gmail.com>
2024-12-28 10:05:30 +0530

    added new feature

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let project=document.querySelector(".project");
 resume.addEventListener("click",()=>{
     window.location.href="resume.html";
 });
-resume.addEventListener("click",()=>{
+project.addEventListener("click",()=>{
     window.location.href="project.html";
 });
